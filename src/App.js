264243cf8580fd18bcf1e8da6380a26d64591304
md-amir-gauhar <gauharamir15@gmail.com>
2021-05-03 12:08:21 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import Article from "./Article";
+
+function App() {
+  const [articles, setArticles] = useState([]);
+  const [subreddit, setSubreddit] = useState('webdev');
+
+  useEffect(() => {
+    fetch(`https://www.reddit.com/r/${subreddit}.json`).then(res => {
+      if (res.status != 200) {
+        console.log("error");
+        return;
+      }
+
+      res.json().then(data => {
+        if (data != null) {
+          setArticles(data.data.children);
+        }
+      });
+    })
+  }, [subreddit]);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <input type="text" className="input" value={subreddit} onChange={e => setSubreddit(e.target.value)} />
+      </header>
+      <div className="articles">
+        {
+          (articles != null) ? articles.map((article, index) => <Article key={index} article={article.data} />) : ''
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
